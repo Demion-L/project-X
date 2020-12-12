23a0cadf1b8c8ac1793b651f0d8c42b546c86d1a
Demion-L <pradovds@ukr.net>
2020-12-12 14:25:54 +0200

    form verification added (almost)

--- js/index.js
+++ js/index.js
@@ -1,67 +1,54 @@
 "use strict";
 
-const personalMovieDB = {
-  count: 0,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
-
-  start: function () {
-    this.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
-
-    while (this.count == "" || this.count == null || isNaN(this.count)) {
-      this.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
-    }
-  },
-  writeYourGenres: function () {
-    for (let i = 1; i < 4; i++) {
-       let genre = prompt(`Ваш любимый жанр под номером ${i}`).toLowerCase();
-
-      if (genre === null || genre === "") {
-         console.log('Эррор! Ведденые данные не верны!');
-        i--;
-      } else {
-         this.genres[i - 1] = genre;
-      }
-    }
-    this.genres.forEach((values, numbers) => {
-      console.log(`Любимый жанр ${numbers + 1} - это ${values}`);
-    });
-  },
-  rememberMyFilms: function () {
-    for (let i = 0; i < 2; i++) {
-      const a = prompt("Один из последних просмотренных фильмов?", ""),
-        b = prompt("На сколько оцените его?", "");
-
-      if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-        this.movies[a] = b;
-        console.log("done!");
-      } else {
-        console.log("error");
-        i--;
-      }
-    }
-  },
-  detectPersonalLevel: function () {
-    if (this.count < 10) {
-      alert("Просмотрено довольно мало фильмов");
-    } else if (this.count >= 10 && this.count < 30) {
-      alert("Вы классический зритель");
-    } else {
-      alert("Вы киноман");
-    }
-  },
-  showMyDB: function (hidden) {
-    if (!hidden) {
-      console.log(personalMovieDB);
-    }
-  },
-  toggleVisibleMyDB: function () {
-    if (this.privat) {
-      this.privat = false;
-    } else {
-      this.privat = true;
-    }
-  },
+document.addEventListener('DOMContentLoaded', () => {
+  const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
 };
+
+const advertiser = document.querySelectorAll('.promo__adv img'),
+      poster = document.querySelector('.promo__bg'),
+      genre = poster.querySelector('.promo__genre'),
+      movieList = document.querySelectorAll('.promo__interactive-list'),
+      addForm = document.querySelector('form.add'),
+      addInput = addForm.querySelector('.adding__input'),
+      checkbox = addForm.querySelector('[type="checkbox"]'),
+      title = poster.querySelector('.promo__title'),
+      ratings = poster.querySelector('.promo__ratings'),
+      promo = document.querySelectorAll('.promo__menu-list');
+      
+
+addForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const newFilm = addInput.value;
+  const favorite = checkbox.checked;
+});
+
+advertiser.forEach(item => {
+    item.remove();
+});
+
+genre.textContent = 'Мелодрамма';
+
+genre.style.fontSize = '30px';
+
+title.textContent = 'Аргонианин';
+
+title.insertAdjacentHTML('afterbegin','<div> Привет хохохо!!! </div>');
+
+poster.style.backgroundImage = 'url(./img/bg.jpg)';
+
+movieList[0].innerHTML = '';
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML +=`<li class="promo__interactive-item"> N${i + 1}${film}<div class="delete"></div></li>`;
+    });
+});
